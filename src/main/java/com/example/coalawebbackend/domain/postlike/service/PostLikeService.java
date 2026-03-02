3be091e173a9dc0c4c10829ae321fb2114317fe1
EZRANDO <gpdnjs2578@naver.com>
2026-03-02 17:04:44 +0900

    [fix] #14 - 사용하지 않는 viewCount 로직 제거

--- src/main/java/com/example/coalawebbackend/domain/postlike/service/PostLikeService.java
+++ src/main/java/com/example/coalawebbackend/domain/postlike/service/PostLikeService.java
@@ -39,9 +39,4 @@ public class PostLikeService {
         long likeCount = postLikeRepository.countByPost(post);
         return PostLikeResponse.of(true, likeCount);
     }
-
-    @Transactional
-    public void increaseViewCount(Post post) {
-        post.increaseViewCount();
-    }
 }
